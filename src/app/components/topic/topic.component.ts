9038a1ac6eaa0109ea7a60e4ec7242afc9b8ee0d
berkaycansakar@gmail.com <berkaycansakar@gmail.com>
2021-07-13 22:57:45 +0300

    servisler oluşturuldu

--- src/app/components/topic/topic.component.ts
+++ src/app/components/topic/topic.component.ts
@@ -1,17 +1,24 @@
+import { TopicService } from './../../services/topic.service';
 import { Topic } from './../../models/topic';
 import { Component, OnInit } from '@angular/core';
+import { TopicResponseModel } from 'src/app/models/topicResponseModel';
 
 @Component({
   selector: 'app-topic',
   templateUrl: './topic.component.html',
-  styleUrls: ['./topic.component.css']
+  styleUrls: ['./topic.component.css'],
 })
 export class TopicComponent implements OnInit {
-  topic:Topic[]=[]
-  
-  constructor() { }
+  topics: Topic[] = [];
+  constructor(private topicService: TopicService) {}
 
   ngOnInit(): void {
+    this.getTopics();
   }
 
+  getTopics() {
+    this.topicService.getTopics().subscribe(response=>{
+      this.topics=response.data
+      })
+  }
 }
